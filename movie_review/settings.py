a9b24d621645efdd20d5875f9056fe48c5574a4c
iliaKhosravi <ilia.khosravi82@gmail.com>
2025-08-11 12:56:28 +0330

    fix CORS

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'movie',
     'user',
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'movie_review.urls'
@@ -137,3 +139,6 @@ REST_FRAMEWORK = {
 # JWT settings
 JWT_ALGORITHM = 'HS256'
 JWT_EXP_HOURS = 24
+
+# Allow all origins for CORS
+CORS_ALLOW_ALL_ORIGINS = True
